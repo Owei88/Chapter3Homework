e9c192b93e1d526c4dacd5b2d284360980c534d1
Owei88 <89204505+Owei88@users.noreply.github.com>
2021-11-26 17:42:12 -0700

    Update StudentTester.java

--- src/StudentTester.java
+++ src/StudentTester.java
@@ -1,14 +1,15 @@
+import java.io.PrintStream;
 public class StudentTester {
 
 
 
         public static void main(String[] args) {
             String StudentName = "Dave";
-            Student student1 = new Student(StudentName, 81);
+            Student student1 = new Student(StudentName);
             student1.addScore(99);
             student1.addScore(69);
-            System.out.println("The Current score is:" + student1.getTotalScore());
-            System.out.println("The Average score is:"+ student1.getTotalScore() / student1.getNumberOfQuizzes());
+            System.out.println("The Current score is: %.2f \n", student1.getTotalScore());
+            System.out.println("The Average score is: %.2f"+ student1.calculateQuizAverage());
 
 
 
