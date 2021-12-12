97edbc363472a16e87107197aec68e17dba7782e
Owei88 <89204505+Owei88@users.noreply.github.com>
2021-12-12 14:57:57 -0700

    Update Microwave.java

--- src/Microwave.java
+++ src/Microwave.java
@@ -1,37 +1,40 @@
 public class Microwave {
+    private double runningTime; // running time of the microwave
+    private int powerLevel; // power level of the microwave
 
-    private int PowerLevel=1;
-    public double MicrowaveTime;
     public Microwave(){
-
+        powerLevel = 1;
     }
-//@param MicrowaveTime sets microwave time
-    //@param PowerLevel decides power level
 
+    /**
+     * resets the running time back to zero
+     */
     public void reset(){
-        MicrowaveTime = 0;
+            runningTime = 0;
+        }
 
+    /**
+     * adds thirty seconds to the running time
+     */
+    public void addThirtySeconds(){
+            runningTime += 30;
+        }
 
-    }
-    public void addThirtySeconds() {
-        MicrowaveTime += 30;
-    }
-
+    /**
+     * Changes the power level from two to one and one to two
+     */
     public void changePowerLevel(){
-        if (PowerLevel == 1){
-            PowerLevel = 2;
+        if (powerLevel == 1){
+            powerLevel = 2;
         }
-        else if (PowerLevel ==2){
-            PowerLevel = 1;
-        }
+        else powerLevel = 1;
     }
 
-    public double getMicrowaveTime(){
-        return MicrowaveTime;
+    /**
+     * prints the microwave start sequence, with the running time and power level
+     */
+    public void startMicrowave(){
+        System.out.println("The microwave will now run for " + runningTime + " seconds at power level " + powerLevel);
     }
 
-    public double getPowerLevel(){
-        return PowerLevel;
-    }
-
-        }
+}
